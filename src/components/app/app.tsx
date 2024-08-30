f15e40a4afde55dc554a70226a7ba4c39de8be83
Jauhien Lipko <nowmdr@protonmail.com>
2024-08-30 18:06:37 +0200

    refactor: add title for ingredient details

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -120,7 +120,7 @@ const App = () => {
           <Route
             path='/ingredients/:id'
             element={
-              <Modal title={'Детали ингредиента'} onClose={closeModal}>
+              <Modal onClose={closeModal}>
                 <IngredientDetails />
               </Modal>
             }
--- src/components/ui/ingredient-details/ingredient-details.tsx
+++ src/components/ui/ingredient-details/ingredient-details.tsx
@@ -9,6 +9,9 @@ export const IngredientDetailsUI: FC<IngredientDetailsUIProps> = memo(
 
     return (
       <div className={styles.content}>
+        <h2 className='text text_type_main-medium mt-5 mb-4'>
+          Детали ингредиента
+        </h2>
         <img
           className={styles.img}
           alt='изображение ингредиента.'
